da6032dfd5b4e486a0084019b27e1aaa05de1474
Saddam-Hossain <Saddam-Hossain@CNU4429H8W.na.odcorp.net>
2015-07-06 03:00:14 -0400

    dup number

---
+++ java/com/sadd/corejava/DuplicateNumber.java
@@ -0,0 +1,14 @@
+package com.sadd.corejava;
+
+public class DuplicateNumber {
+
+    public static void main(String[] args) {
+        
+        for (int i=0;i>=10;i++){
+            
+        }
+    }
+    
+    
+
+}
