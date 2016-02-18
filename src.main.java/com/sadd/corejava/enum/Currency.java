ec13fb1cd8f46268dd07b30e3aad53de55027df3
Hossain, Sadd <Sadd_Hossain@premierinc.com>
2016-02-18 14:20:56 -0500

    no message

---
+++ src.main.java/com/sadd/corejava/enum/Currency.java
@@ -0,0 +1,8 @@
+package com.sadd.corejava.enum;
+
+public enum Currency
+
+{
+    PENNY,DIME,QUARTER,NICKLE
+
+}
---
+++ src.main.java/com/sadd/corejava/enum/EnumRunner.java
@@ -0,0 +1,13 @@
+package com.sadd.corejava.enum;
+
+
+public class EnumRunner{
+	
+	public static void main(String[] args){
+		
+		
+	}
+
+    System.out.print("Currency is");
+
+}
